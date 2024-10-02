acbbbbb56f673147dcb437545c14b55bc5c20fe0
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-03 00:53:43 +0300

    SEOTLT feat:created crud api with promise

---
+++ src/features/newsPage/api/newsApi.ts
@@ -0,0 +1,46 @@
+import { EditNewsItem, NewsItem } from '../model/newsSlice.ts'
+import { loadNews } from '../../../utils/lockaStorage/lockaStorage.ts'
+
+const initNews = [
+  {
+    id: '1',
+    title: 'Some news',
+    content:
+      '        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nisi optio saepe ut? Ab\n' +
+      '        accusamus, asperiores consectetur, consequatur, doloribus eum id inventore nostrum odit\n' +
+      '        provident qui repudiandae sequi sit voluptatum.',
+  },
+]
+
+const persistedNews = loadNews()
+
+export const newsApi = {
+  fetchNews: () => {
+    return new Promise<NewsItem[]>(resolve => {
+      setTimeout(() => {
+        resolve(persistedNews || initNews)
+      }, 500)
+    })
+  },
+  addNews: (newsItem: NewsItem) => {
+    return new Promise<NewsItem>(resolve => {
+      setTimeout(() => {
+        resolve(newsItem)
+      }, 500)
+    })
+  },
+  editNews: (data: EditNewsItem) => {
+    return new Promise<EditNewsItem>(resolve => {
+      setTimeout(() => {
+        resolve(data)
+      }, 500)
+    })
+  },
+  deleteNews: (id: string) => {
+    return new Promise<string>(resolve => {
+      setTimeout(() => {
+        resolve(id)
+      }, 500)
+    })
+  },
+}
