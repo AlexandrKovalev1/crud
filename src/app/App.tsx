0a1bb21b81519d34d5867a58e83d5b6d5c80294c
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-02 14:43:58 +0300

    SEOTLT feat: create layout

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { NewsPage } from '../features/newsPage/ui'
+import { Layout } from '../components/layout'
+
+function App() {
+  return (
+    <Layout>
+      <NewsPage />
+    </Layout>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './app/App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './services/store.ts'
 import './styles/index.scss'
