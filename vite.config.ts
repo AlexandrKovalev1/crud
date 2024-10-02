a83e71f4e0c58eea0c99cf07945cc69e7becc4e8
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-03 00:48:25 +0300

    SEOTLT build: add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://alexandrkovalev1.github.io/crud/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
