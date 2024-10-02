45a581502c1fc5f651f7c4fa724693d6fb4a272d
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-02 08:40:38 +0300

    SEOTLT feat: add boilerplate styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './services/store.ts'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
