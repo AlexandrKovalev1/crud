6469d1250aa966dafe59aaf1d46ad515a0795f3e
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-03 00:54:33 +0300

    SEOTLT refactor:configured store

--- src/services/store.ts
+++ src/services/store.ts
@@ -2,21 +2,17 @@ import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { newsSlice } from '../features/newsPage/model/newsSlice.ts'
 import { appSlice } from '../app/appSlice.ts'
-import { loadState, saveState } from '../utils/lockaStorage/lockaStorage.ts'
+import { saveNews } from '../utils/lockaStorage/lockaStorage.ts'
 
-const persistedState = loadState()
 export const store = configureStore({
   reducer: {
     app: appSlice,
     news: newsSlice,
   },
-  preloadedState: {
-    news: persistedState,
-  },
 })
 
 store.subscribe(() => {
-  saveState(store.getState())
+  saveNews(store.getState())
 })
 
 export const useAppDispatch = useDispatch<typeof store.dispatch>
