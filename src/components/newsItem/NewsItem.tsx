cb34e50c89ee1ff37ad620cd42cccab69420affc
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-02 17:19:26 +0300

    SEOTLT init: start NewsItem

---
+++ src/components/newsItem/NewsItem.tsx
@@ -0,0 +1,13 @@
+type Props = {}
+export const NewsItem = (props: Props) => {
+  return (
+    <div>
+      <h2>Heading</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque, beatae corporis
+        distinctio, facilis illum ipsam iure magnam minus officiis optio praesentium quaerat, sequi
+        temporibus unde voluptas voluptate. Incidunt, ipsa.
+      </p>
+    </div>
+  )
+}
