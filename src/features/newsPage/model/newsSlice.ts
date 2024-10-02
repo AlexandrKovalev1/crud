3702878224c9bb700395092e02f85fe00622e3c8
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-03 01:06:29 +0300

    SEOTLT feat: implemented news CRUD actions

--- src/features/newsPage/model/newsSlice.ts
+++ src/features/newsPage/model/newsSlice.ts
@@ -1,33 +1,71 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { asyncThunkCreator, buildCreateSlice, PayloadAction } from '@reduxjs/toolkit'
 import { v4 as uuidv4 } from 'uuid'
+import { newsApi } from '../api/newsApi.ts'
 
-const slice = createSlice({
+const createAppSlice = buildCreateSlice({
+  creators: { asyncThunk: asyncThunkCreator },
+})
+
+const slice = createAppSlice({
   name: 'news',
   reducers: creators => {
     return {
-      addNewsItem: creators.reducer((state, action: PayloadAction<Omit<NewsItem, 'id'>>) => {
-        const id = uuidv4()
-
-        state.push({ ...action.payload, id })
-      }),
-      deleteNewsItem: creators.reducer((state, action: PayloadAction<{ id: string }>) => {
-        return state.filter(n => n.id !== action.payload.id)
-      }),
-      fetchNews: creators.reducer((_, action: PayloadAction<NewsItem[]>) => {
-        return action.payload
-      }),
-      editNewsItem: (state, action: PayloadAction<EditNewsItem>) => {
-        let currentNewsItem = state.find(n => n.id === action.payload.id)
-
-        if (currentNewsItem) {
-          currentNewsItem = Object.assign(currentNewsItem, action.payload)
+      fetchNews: creators.asyncThunk<NewsItem[]>(
+        async () => {
+          return await newsApi.fetchNews()
+        },
+        {
+          fulfilled: (_, action) => {
+            return action.payload
+          },
         }
+      ),
+      addNewsItem: creators.asyncThunk<NewsItem, Omit<NewsItem, 'id'>>(
+        async (newsItem: Omit<NewsItem, 'id'>) => {
+          const id = uuidv4()
+          const res = await newsApi.addNews({ ...newsItem, id })
+          console.log(res)
+          return { ...res }
+        },
+        {
+          fulfilled: (state, action) => {
+            state.push(action.payload)
+          },
+        }
+      ),
 
-        return state
-      },
+      deleteNewsItem: creators.asyncThunk<string, string>(
+        async (id: string) => {
+          return await newsApi.deleteNews(id)
+        },
+        {
+          fulfilled: (state, action: PayloadAction<string>) => {
+            return state.filter(n => n.id !== action.payload)
+          },
+        }
+      ),
+      editNewsItem: creators.asyncThunk<EditNewsItem, EditNewsItem>(
+        async (data: EditNewsItem) => {
+          return await newsApi.editNews(data)
+        },
+        {
+          fulfilled: (state, action: PayloadAction<EditNewsItem>) => {
+            let currentNewsItem = state.find(n => n.id === action.payload.id)
+
+            if (currentNewsItem) {
+              currentNewsItem = Object.assign(currentNewsItem, action.payload)
+            }
+
+            return state
+          },
+        }
+      ),
     }
   },
   initialState: [] as NewsItem[],
+  selectors: {
+    selectNews: state => state,
+  },
 })
 
 export type NewsItem = {
@@ -38,3 +76,4 @@ export type NewsItem = {
 export type EditNewsItem = Omit<Partial<NewsItem>, 'id'> & { id: string }
 export const newsSlice = slice.reducer
 export const newsActions = slice.actions
+export const { selectNews } = slice.selectors
