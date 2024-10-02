53bf569bebd11f3d76c2fa619a3e340cfea72415
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-03 00:51:06 +0300

    SEOTLT feat: create modal component

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,21 @@
-type Props = {}
-export const Modal = (props: Props) => {
-  return <div></div>
+import * as Dialog from '@radix-ui/react-dialog'
+import { ReactNode } from 'react'
+import s from './modal.module.scss'
+
+type Props = {
+  trigger?: ReactNode
+  content?: ReactNode
+  open?: boolean
+  setOpen: (isOpen: boolean) => void
+}
+export const Modal = ({ trigger, open, setOpen, content }: Props) => {
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      {trigger && <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>}
+      <Dialog.Portal>
+        <Dialog.Overlay className={s.overlay} />
+        <Dialog.Content className={s.content}>{content}</Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
 }
