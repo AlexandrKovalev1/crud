c51ca71cd47703d43b637fa667b58a29fce85d96
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-03 01:07:45 +0300

    SEOTLT feat: implemented newsItem component

--- src/components/newsItem/NewsItem.tsx
+++ src/components/newsItem/NewsItem.tsx
@@ -1,13 +1,39 @@
-type Props = {}
-export const NewsItem = (props: Props) => {
+import s from './newsItem.module.scss'
+import { Button } from '../button'
+import { TrashIcon } from '@radix-ui/react-icons'
+import { EditNewsItem } from '../editNewsItem'
+import { useAppDispatch } from '../../services/store.ts'
+import { newsActions } from '../../features/newsPage/model/newsSlice.ts'
+
+type Props = {
+  heading?: string
+  content?: string
+  id: string
+}
+export const NewsItem = ({ heading = 'Без заголовка', content = '', id }: Props) => {
+  const dispatch = useAppDispatch()
+
+  const onDeleteClickHandler = () => {
+    dispatch(newsActions.deleteNewsItem(id))
+  }
+
   return (
-    <div>
-      <h2>Heading</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque, beatae corporis
-        distinctio, facilis illum ipsam iure magnam minus officiis optio praesentium quaerat, sequi
-        temporibus unde voluptas voluptate. Incidunt, ipsa.
-      </p>
+    <div className={s.wrapper}>
+      <div className={s.contentBlock}>
+        <h3>{heading}</h3>
+        <p>{content}</p>
+      </div>
+      <div className={s.buttonsBlock}>
+        <Button
+          variant={'delete'}
+          className={s.trash}
+          title={'Удалить новость'}
+          onClick={onDeleteClickHandler}
+        >
+          <TrashIcon />
+        </Button>
+        <EditNewsItem newsTitle={heading} newsContent={content} id={id} />
+      </div>
     </div>
   )
 }
