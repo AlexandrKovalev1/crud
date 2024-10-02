1fc854ce54e061f868269bf88678ed2f1ed5cc95
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-03 01:04:18 +0300

    SEOTLT feat: implemented news CRUD tests

--- src/features/newsPage/model/__tests__/newsSlice.test.ts
+++ src/features/newsPage/model/__tests__/newsSlice.test.ts
@@ -14,12 +14,39 @@ describe('news slice CRUD tests', () => {
     ]
   })
 
+  it('should fetch a news ', () => {
+    type FetchNewsItemActionType = Omit<ReturnType<typeof newsActions.fetchNews.fulfilled>, 'meta'>
+
+    const fetchedNews = [
+      {
+        id: uuidv4(),
+        title: 'News',
+        content: 'Some content news',
+      },
+      {
+        id: uuidv4(),
+        title: 'Another News',
+        content: 'Another content news',
+      },
+    ]
+
+    const action: FetchNewsItemActionType = {
+      type: newsActions.fetchNews.fulfilled.type,
+      payload: fetchedNews,
+    }
+
+    const newState = newsSlice([], action)
+
+    expect(newState).toEqual(fetchedNews)
+    expect(newState.length).toBe(2)
+  })
+
   it('should add a news item', () => {
-    type AddNewsItemActionType = Omit<ReturnType<typeof newsActions.addNewsItem>, 'meta'>
+    type AddNewsItemActionType = Omit<ReturnType<typeof newsActions.addNewsItem.fulfilled>, 'meta'>
 
     const action: AddNewsItemActionType = {
-      type: newsActions.addNewsItem.type,
-      payload: { content: 'Test content', title: 'Test title' },
+      type: newsActions.addNewsItem.fulfilled.type,
+      payload: { content: 'Test content', title: 'Test title', id: uuidv4() },
     }
 
     const newState = newsSlice(initialState, action)
@@ -29,39 +56,28 @@ describe('news slice CRUD tests', () => {
   })
 
   it('should delete a news item', () => {
-    type DeleteNewsItemActionType = Omit<ReturnType<typeof newsActions.deleteNewsItem>, 'meta'>
+    type DeleteNewsItemActionType = Omit<
+      ReturnType<typeof newsActions.deleteNewsItem.fulfilled>,
+      'meta'
+    >
 
     const action: DeleteNewsItemActionType = {
-      type: newsActions.deleteNewsItem.type,
-      payload: { id: initialState[0].id },
+      type: newsActions.deleteNewsItem.fulfilled.type,
+      payload: initialState[0].id,
     }
 
     const newState = newsSlice(initialState, action)
     expect(newState.length).toBe(0)
   })
 
-  it('should fetch news items', () => {
-    const newsItems = [
-      { id: '1', content: 'Test content 1', title: 'Test title 1' },
-      { id: '2', content: 'Test content 2', title: 'Test title 2' },
-    ]
-
-    type FetchNewsActionType = Omit<ReturnType<typeof newsActions.fetchNews>, 'meta'>
-
-    const action: FetchNewsActionType = {
-      type: newsActions.fetchNews.type,
-      payload: newsItems,
-    }
-
-    const newState = newsSlice(initialState, action)
-    expect(newState).toEqual(newsItems)
-  })
-
   it('should edit a news item title', () => {
-    type EditNewsItemActionType = Omit<ReturnType<typeof newsActions.editNewsItem>, 'meta'>
+    type EditNewsItemActionType = Omit<
+      ReturnType<typeof newsActions.editNewsItem.fulfilled>,
+      'meta'
+    >
 
     const action: EditNewsItemActionType = {
-      type: newsActions.editNewsItem.type,
+      type: newsActions.editNewsItem.fulfilled.type,
       payload: { id: initialState[0].id, title: 'New title' },
     }
 
@@ -72,10 +88,13 @@ describe('news slice CRUD tests', () => {
   })
 
   it('should edit a news item content', () => {
-    type EditNewsItemActionType = Omit<ReturnType<typeof newsActions.editNewsItem>, 'meta'>
+    type EditNewsItemActionType = Omit<
+      ReturnType<typeof newsActions.editNewsItem.fulfilled>,
+      'meta'
+    >
 
     const action: EditNewsItemActionType = {
-      type: newsActions.editNewsItem.type,
+      type: newsActions.editNewsItem.fulfilled.type,
       payload: { id: initialState[0].id, content: 'New content' },
     }
 
